4a0f106983095e8c32b8333c4a3887853e726cbb
masienz <85255238+masienz@users.noreply.github.com>
2022-12-12 17:22:20 +0300

    Waterfall ground added to script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,17 @@ using static UnityEditor.Progress;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> waterfalls;
-    [SerializeField] private float normalVoroniSpeed = 0.3f;
-	[SerializeField] private float highVoroniSpeed = 0.6f;
+	[Header("WaterfallsGround")]
+	[SerializeField] private List<GameObject> waterfallsGround;
+	[SerializeField] private float normalVoroniSpeedOnGround = 0.15f;
+	[SerializeField] private float highVoroniSpeedOnGround = 0.3f;
 
+	private float currentSpeedOnGround;
+
+	[Header("Waterfalls")]
+	[SerializeField] private List<GameObject> waterfalls;
+	[SerializeField] private float normalVoroniSpeed = 0.3f;
+	[SerializeField] private float highVoroniSpeed = 0.6f;
 	[SerializeField] private float woobbleAmountNormal = 0;
 	[SerializeField] private float woobbleAmountHigh = 0.1f;
 	[SerializeField] private float woobbleFrequancyNormal = 0;
@@ -28,21 +35,30 @@ public class Player : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
+			//Waterfalls
 			currentSpeed = highVoroniSpeed;
 			currentWoobbleAmount = woobbleAmountHigh;
 			currentWoobbleFrequancy = woobbleFrequancyHigh;
 			currentWoobbleHeight = woobbleHeightHigh;
 			currentWoobbleSpeed = woobbleSpeedHigh;
+
+			//Waterfalls On Ground
+			currentSpeedOnGround = highVoroniSpeedOnGround;
 		}
         else
-        {
+		{
+			//Waterfalls
 			currentSpeed = normalVoroniSpeed;
 			currentWoobbleAmount = woobbleAmountNormal;
 			currentWoobbleFrequancy = woobbleFrequancyNormal;
 			currentWoobbleHeight = woobbleHeightNormal;
 			currentWoobbleSpeed = woobbleSpeedNormal;
+
+			//Waterfalls On Ground
+			currentSpeedOnGround = highVoroniSpeedOnGround;
 		}
 
+		//Waterfalls
 		foreach (var item in waterfalls)
 		{
 			item.GetComponent<MeshRenderer>().material.SetVector("_VoroniSpeed", new Vector2(0, currentSpeed));
@@ -50,6 +66,12 @@ public class Player : MonoBehaviour
 			item.GetComponent<MeshRenderer>().material.SetFloat("_WoobbleFrequancy", currentWoobbleFrequancy);
 			item.GetComponent<MeshRenderer>().material.SetFloat("_WoobbleHeight", currentWoobbleHeight);
 			item.GetComponent<MeshRenderer>().material.SetFloat("_WoobbleSpeed", currentWoobbleSpeed);
+		}
+
+		//Waterfalls On Ground
+		foreach (var item in waterfallsGround)
+		{
+			item.GetComponent<MeshRenderer>().material.SetVector("_VoroniSpeed", new Vector2(0, currentSpeedOnGround));
 		}
 	}
 }
